963a4d64d5b4efbc66833edb3ae5afebb0425b21
Cynthia Nezames <cnezames@gmail.com>
2018-08-01 19:13:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,11 +25,9 @@ function dec(a) {
 }
 
 function makeInt(a) {
-  parseInt('a', 10)
-  return 'a'
+  return parseInt('a', 10)
 }
 
 function preserveDecimal(a) {
-  parseFloat('a')
-  return a
+  return parseFloat('a')
 }
